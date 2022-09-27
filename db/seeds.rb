b913ceb8d51f407dbf678793dee4782d373d7b83
Hori Kouta <kouta2880@gmail.com>
2022-09-27 10:36:28 +0900

    不要となったsample_articlesのmigrationファイルを削除しました。 テストデータを作成する為にseedファイルを実装しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 3.times do |n|
+#   User.create!(
+#     email: "user00#{n + 1}@test.com",
+#     password: "test1234"
+#   )
+# end
+
+# user = User.find_by(id: 1)
+# 50.times do |n|
+#   user.articles.create!(
+#   title: "No.#{n + 1}: user00#{user.id}の記事",
+#   content: "No.#{n + 1}: user00#{user.id}の記事の本文",
+#   )
+# end
+# user = User.find_by(id: 2)
+# 50.times do |n|
+#   user.articles.create!(
+#   title: "No.#{n + 1}: user00#{user.id}の記事",
+#   content: "No.#{n + 1}: user00#{user.id}の記事の本文",
+#   )
+# end
+# user = User.find_by(id: 3)
+# 50.times do |n|
+#   user.articles.create!(
+#   title: "No.#{n + 1}: user00#{user.id}の記事",
+#   content: "No.#{n + 1}: user00#{user.id}の記事の本文",
+#   )
+# end
+
+
+3.times do |n|
+  user = User.create!(
+    email: "user00#{n + 1}@test.com",
+    password: "test1234"
+  )
+
+
+    50.times do |n|
+      user.articles.create!(
+
+      title: "No.#{n + 1}: user00#{user.id}の記事",
+      content: "No.#{n + 1}: user00#{user.id}の記事の本文",
+      )
+    end
+end
+
+# 50.times do |n|
+#   user.articles.create!(
+
+#   title: "No.#{n + 1}: user00#{user.id}の記事",
+#   content: "No.#{n + 1}: user00#{user.id}の記事の本文",
+#   )
+# end
