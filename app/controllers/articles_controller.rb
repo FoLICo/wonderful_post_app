43ac72803a4e2826a9d2915c0e246e142de632e2
Hori Kouta <kouta2880@gmail.com>
2022-09-28 09:09:22 +0900

    検索フォームを記事一覧画面とマイページに実装しました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,14 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    # @articles = Article.all.page(params[:page])
+    # @articles = Article.where("title LIKE ?", "%#{params[:title]}%")
+
+    if params[:title]
+      @articles = Article.where("title LIKE ?", "%#{params[:title]}%").page(params[:page])
+    else
+      @articles = Article.all.page(params[:page])
+    end
   end
 
   # GET /articles/1 or /articles/1.json
--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,7 +2,14 @@ class MypagesController < ApplicationController
 
   def index
     # binding.pry
-    @articles = current_user.articles.all
+    @articles = current_user.articles.all.page(params[:page])
+
+    if params[:title]
+      @articles = current_user.articles.where("title LIKE ?", "%#{params[:title]}%").page(params[:page])
+    else
+      @articles = current_user.articles.all.page(params[:page])
+    end
+
   end
 
 end
